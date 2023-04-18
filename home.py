1a261cce801776c1e8a1916b22e90687f072ae33
Sanket Bairagi <89782152+SanketBairagi@users.noreply.github.com>
2023-04-18 19:58:26 +0530

    Update home.py

--- home.py
+++ home.py
@@ -82,8 +82,8 @@ if selected=="Data And Analysis":
      )
     st.header("About Dataset")
     st.markdown(""" 
-    **2020 annual CDC survey data of 400k adults related to their health status**  
-    [Download Dataset](https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease?datasetId=1936563) \n
+    **2020 annual CDC survey data of 400k adults related to their health status**  \n
+    
     **What topic does the dataset cover?** \n
     According to the CDC, heart disease is one of the leading causes of death for people of most races in the US (African Americans, American Indians and Alaska Natives, and white people). About half of all Americans (47%) have at least 1 of 3 key risk factors for heart disease: high blood pressure, high cholesterol, and smoking. Other key indicator include diabetic status, obesity (high BMI), not getting enough physical activity or drinking too much alcohol. Detecting and preventing the factors that have the greatest impact on heart disease is very important in healthcare. Computational developments, in turn, allow the application of machine learning methods to detect "patterns" from the data that can predict a patient's condition.
 
@@ -98,7 +98,7 @@ if selected=="Data And Analysis":
 
     df=pd.read_csv("https://raw.githubusercontent.com/SanketBairagi/final_project/main/heart_2020_cleaned.csv")
     st.dataframe(df)
-
+    st.markdown(""" [Download Dataset](https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease?datasetId=1936563) """)
 
 if selected=="Reports":
     st.markdown(
